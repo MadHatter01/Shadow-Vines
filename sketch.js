7aac41858ef47a5a439cd1f2fc3d524401fd384f
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-11-10 21:27:28 -0800

    background setup

--- sketch.js
+++ sketch.js
@@ -1,4 +1,36 @@
+let player;
+let platforms = [];
+let gameControl;
+
+
 function setup(){
-    createCanvas(800, 400);
+    createCanvas(800, 500);
+    
+    rectMode(CENTER);
+    textAlign(CENTER)
+    player = {
+        x:400,
+        y:375,
+        width: 30,
+        height:70
+    }
+    gameControl = {
+        stage:1
+    }
+
+    platforms.push({x:200, y:300, width:200, height:40});
+    
+}
+
+function draw(){
+    if (gameControl.stage == 1){
+        game();
+    }
+}
+
+function game(){
     background(20, 30, 20);
+    noStroke();
+    fill(100, 200,75);
+    rect(width/2, 450, width, 100);
 }
