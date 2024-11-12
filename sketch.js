f821eb52d70077cf9dc9a054953c7ef183ea59f4
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-11-11 17:28:24 -0800

    movement logic

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let player;
 let platforms = [];
 let gameControl;
+let playerJump = false;
 
 
 function setup(){
@@ -25,6 +26,7 @@ function setup(){
 function draw(){
     if (gameControl.stage == 1){
         game();
+        keyPressed();
     }
 }
 
@@ -33,4 +35,38 @@ function game(){
     noStroke();
     fill(100, 200,75);
     rect(width/2, 450, width, 100);
+    noFill();
+    stroke(0)
+    strokeWeight(20);
+    rect(width/2, height/2, width, height);
+
+    // platform
+    stroke(0);
+    strokeWeight(5);
+    fill(255,120,0);
+    rect(platforms[0].x, platforms[0].y, platforms[0].width, platforms[0].height);
+
+
+    //player
+    stroke(0);
+    fill(100, 0, 255);
+    rect(player.x, player.y, player.width, player.height);
+
+}
+
+function keyPressed(){
+    if (keyCode === LEFT_ARROW){
+        player.x -=5;
+    }
+    if (keyCode === RIGHT_ARROW){
+        player.x +=5;
+    }
+
+    if(keyCode === UP_ARROW){
+        playerJump = true;
+    }
+    
+    if(keyCode === DOWN_ARROW){
+        playerJump = false;
+    }
 }
